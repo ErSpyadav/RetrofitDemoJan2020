6f7241a37b413b38be2b2a414221e0fd1ce736f1
SarayuYadaw <sarary264@gmail.com>
2020-01-19 14:43:46 +0530

    initiale commit

---
+++ app/src/main/java/demo/pushnotification/retrofitapp2020/MainActivity.java
@@ -0,0 +1,14 @@
+package demo.pushnotification.retrofitapp2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
